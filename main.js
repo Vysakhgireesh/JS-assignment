7aac71a3b011feef9e2c3ec10fa2d03dd5b260f0
Vysakh Gireesh <vysakhgireesh07@gmail.com>
2024-07-25 13:09:13 +0530

    initial commit

---
+++ main.js
@@ -0,0 +1,12 @@
+
+
+let number = parseInt(prompt("Enter a number between 0 and 1 million."));
+let Sum = 0;
+if (number > 0 && number <= 1000000) {
+    for (let i = 1; i <= number; i++) {
+        Sum += i;
+    } document.write(`Sum of numbers up to ${number} is ${Sum}`)
+
+} else {
+    document.write("Number is invalid")
+}
